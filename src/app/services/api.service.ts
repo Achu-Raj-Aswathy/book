7e47433c9fb1fe72c728bb5bbbb10d70396da32f
Achu-Raj-Aswathy <achurajaswathy@gmail.com>
2024-08-05 23:41:00 +0530

    storeBase

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  server_url = "https://bookstore-server-xdvo.onrender.com";
+  server_url = "https://book-server-de01.onrender.com";
 
   constructor(private http: HttpClient) { }
 
